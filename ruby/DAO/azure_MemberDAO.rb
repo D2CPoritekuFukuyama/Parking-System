25d57f3335bd997ab1a2747536b3b546e7990d12
D2CPoritekuFukuyama <d2c_poriteku@yahoo.co.jp>
2016-10-24 23:50:21 +0900

    MemberDAOの修正

--- ruby/DAO/azure_MemberDAO.rb
+++ ruby/DAO/azure_MemberDAO.rb
@@ -3,25 +3,17 @@ require './DAO/azure_baseDAO.rb'
 
 class Member_DAO < Azure_DAO 
     def get_member(number, cate_num, hiragana, area)
-#        sql = "SELECT COUNT(PLATENo1), COUNT(PLATENo2), COUNT(Area), COUNT(hiragana) 
-#                FROM 会員
-#                WHRE PLATENo2 = '#{number[0] + number[1] + '-' + number[2] + number[3]}'
-#                AND PLATENo1 = '300'
-#                AND Area = (N'#{area.sub(/\s/,'')}')
-#                AND hiragana = 'hiragana';"
-        sql = "SELECT PLATENo1, PLATENo2, Area, hiragana 
+        sql = "SELECT 会員ID,PLATENo1, PLATENo2, Area, hiragana 
                 FROM 会員 
                 WHERE PLATENo1 = '#{cate_num.to_i}'
-                AND PLATENo2 = '#{number}';"
-#                AND Area = (N'#{area.sub(/\s/,'')}')
+                AND PLATENo2 = '#{number}'
+                AND Area = (N'#{area}');"
 #                AND hiragana = 'hiragana';" 
         results = @client.execute(sql)
-        puts results.count 
 #        results.each do |row|
 #            puts row
 #        end
+        return results
     end
 end
 
-#test = Member_DAO.new
-#test.get_member
