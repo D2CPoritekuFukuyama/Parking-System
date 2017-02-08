10c15d4847a65c727b6432f3484e0e1e492f3d7a
D2CPoritekuFukuyama <d2c_poriteku@yahoo.co.jp>
2017-02-08 13:30:59 +0900

    デバッグ用コードをコメントアウト

--- ruby/DAO/azure_MemberDAO.rb
+++ ruby/DAO/azure_MemberDAO.rb
@@ -5,7 +5,7 @@ class Member_DAO < Azure_DAO
     def get_member(number, cate_num, hiragana, area)
 #        sql = "SELECT 会員ID,PLATENo, Area, hiragana 
 #                FROM 会員"
-        puts "hiragana:#{hiragana}"
+#        puts "hiragana:#{hiragana}"
         sql = "SELECT Nplate_ID,number, area, hiragana 
                 FROM members_Nplate
                 WHERE number = '#{number.to_i}'
@@ -19,8 +19,8 @@ class Member_DAO < Azure_DAO
     end
 end
 
-member = Member_DAO.new
-results = member.get_member("2456", "301", "to", "福山")
-results.each do |row|
-    puts row
-end
+#member = Member_DAO.new
+#results = member.get_member("2456", "301", "to", "福山")
+#results.each do |row|
+#    puts row
+#end
