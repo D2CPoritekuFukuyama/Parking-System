7527f9a85a9bc1c7901e410dc46f316136743236
ishikawa-pro <ishikawa.pro@gmail.com>
2017-02-09 16:05:52 +0900

    [fix]オンプレ上のsqlserverへの接続用クラス HPのノートパソコン上のsqlserverへの接続に対応
    requirement params
    username: sqlserver認証のユーザー名
    passowrd: sqlserver認証のパスワード
    dataserver: IPadress:1433
    database: データベース名(ParkingSystem)
    
    以上を環境変数に追加する必要がある

--- ruby/DAO/azure_baseDAO.rb
+++ ruby/DAO/azure_baseDAO.rb
@@ -3,12 +3,12 @@ require 'tiny_tds'
 class Azure_DAO
     attr_reader :clinet
     def initialize()
-        @client = TinyTds::Client.new username: ENV['SQL_DATABASE_USERNAME'], password: ENV['SQL_DATABASE_PASSWORD'], host: ENV['SQL_DATABASE_HOST']
+        @client = TinyTds::Client.new username: ENV['SQL_DATABASE_USERNAME'], password: ENV['SQL_DATABASE_PASSWORD'], dataserver: ENV['SQL_DATABASE_HOST'],database: ENV['SQL_DATABASE_NAME'], login_timeout: '5'
             
     end
 
     def get_table()
-        sql = "SELECT name FROM sysobjects WHERE xtype = 'U';"
+        sql = "SELECT * FROM dbo.会員;"
         results = @client.execute(sql)
         results.each do |row|
             puts row 
